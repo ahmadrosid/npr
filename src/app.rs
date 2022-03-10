4ac54e6d40a8e4a588be24c41b71dad663de11d5
ahmadrosid <alahmadrosid@gmail.com>
2022-03-10 15:43:47 +0700

    refactor: migrate rendering using tui-rs

---
+++ src/app.rs
@@ -0,0 +1,148 @@
+use crate::document::Document;
+use crossterm::event::{self, Event, KeyCode};
+use std::{io, process::Command};
+use tui::{
+    backend::Backend,
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    Frame, Terminal,
+};
+use unicode_width::UnicodeWidthStr;
+
+pub struct App {
+    should_run_script: bool,
+    index: usize,
+    input: String,
+    messages: Vec<String>,
+    doc: Document,
+}
+
+impl App {
+    pub fn new(messages: Vec<String>, doc: Document) -> Self {
+        Self {
+            should_run_script: false,
+            index: 0,
+            input: String::new(),
+            messages,
+            doc,
+        }
+    }
+
+    pub fn run_search(&mut self) {
+        self.messages = self.doc.search(&self.input);
+    }
+
+    pub fn run_script(&self) {
+        if !self.should_run_script {
+            return;
+        }
+
+        if let Some(key) = self.messages.get(self.index) {
+            if let Some(script) = self.doc.get_script(key) {
+                let child = Command::new("npm").arg("run").arg(script).spawn();
+                let _ = child.unwrap();
+            }
+        }
+    }
+
+    fn udpate_index(&mut self, code: KeyCode) {
+        match code {
+            KeyCode::Up => {
+                if self.index == 0 {
+                    return;
+                }
+                self.index -= 1;
+            }
+            KeyCode::Down => {
+                if self.index == self.messages.len() - 1 {
+                    return;
+                }
+                self.index += 1;
+            }
+            _ => {}
+        };
+    }
+}
+
+pub fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<App> {
+    loop {
+        terminal.draw(|f| ui(f, &app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Enter => {
+                    app.should_run_script = true;
+                    return Ok(app);
+                }
+                KeyCode::Char(c) => {
+                    app.input.push(c);
+                    app.run_search();
+                }
+                KeyCode::Backspace => {
+                    app.input.pop();
+                    app.run_search();
+                }
+                KeyCode::Up | KeyCode::Down => {
+                    app.udpate_index(key.code);
+                }
+                KeyCode::Esc => {
+                    return Ok(app);
+                }
+                _ => {}
+            }
+        }
+    }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, app: &App) {
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints(
+            [
+                Constraint::Length(1),
+                Constraint::Length(3),
+                Constraint::Min(1),
+            ]
+            .as_ref(),
+        )
+        .split(f.size());
+
+    let msg = vec![
+        Span::raw("Press "),
+        Span::styled("Esc", Style::default().add_modifier(Modifier::BOLD)),
+        Span::raw(" to close sesssion, "),
+        Span::styled("Enter", Style::default().add_modifier(Modifier::BOLD)),
+        Span::raw(" to execute script"),
+    ];
+
+    let mut text = Text::from(Spans::from(msg));
+    text.patch_style(Style::default());
+    let help_message = Paragraph::new(text);
+    f.render_widget(help_message, chunks[0]);
+
+    let input = Paragraph::new(app.input.as_ref())
+        .style(Style::default().fg(Color::Yellow))
+        .block(Block::default().borders(Borders::ALL).title("Search"));
+    f.render_widget(input, chunks[1]);
+    f.set_cursor(chunks[1].x + app.input.width() as u16 + 1, chunks[1].y + 1);
+
+    let messages: Vec<ListItem> = app
+        .messages
+        .iter()
+        .enumerate()
+        .map(|(i, m)| {
+            let content = vec![Spans::from(Span::raw(m))];
+            if i == app.index {
+                ListItem::new(content).style(Style::default().bg(Color::Gray).fg(Color::Black))
+            } else {
+                ListItem::new(content)
+            }
+        })
+        .collect();
+    let messages =
+        List::new(messages).block(Block::default().borders(Borders::ALL).title("Result"));
+    f.render_widget(messages, chunks[2]);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,76 +1,47 @@
+mod app;
 mod document;
 
-use std::{
-    cmp::{max, min},
-    process::Command,
+use crossterm::{
+    event::{DisableMouseCapture, EnableMouseCapture},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use tuikit::prelude::*;
+use std::{error::Error, io};
+use tui::{backend::CrosstermBackend, Terminal};
 
+use app::App;
 use document::Document;
 
-fn main() {
-    let term: Term<()> = Term::new().unwrap();
-    let mut row = 1;
-    let mut query = String::new();
+fn main() -> Result<(), Box<dyn Error>> {
     let doc = Document::parse_script();
     if let Err(ref err) = doc {
-        let attr = Attr {
-            fg: Color::RED,
-            ..Attr::default()
-        };
-        let _ = term.print_with_attr(row, 0, &err, attr);
+        println!("{}", err);
+        std::process::exit(1);
     };
+    let doc = doc.unwrap();
 
-    let mut data = doc.as_ref().unwrap().search(&query);
-    let mut max_height = data.len();
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
 
-    while let Ok(ev) = term.poll_event() {
-        match ev {
-            Event::Key(Key::ESC) => break,
-            Event::Key(Key::Enter) => {
-                if let Some(key) = data.get(row - 1) {
-                    if let Some(script) = doc.as_ref().unwrap().get_script(key) {
-                        let child = Command::new("npm").arg("run").arg(script).spawn();
-                        let _ = child.unwrap();
-                    }
-                }
-                break;
-            }
-            Event::Key(Key::Up) => row = max(row - 1, 1),
-            Event::Key(Key::Down) => row = min(row + 1, max_height),
-            Event::Key(Key::Char(ch)) => query.push(ch),
-            Event::Key(Key::Backspace) => {
-                if !query.is_empty() {
-                    query.truncate(query.len() - 1)
-                }
-            }
-            _ => {}
-        }
+    let scripts = doc.search("");
+    let app = App::new(scripts, doc);
+    let res = app::run_app(&mut terminal, app);
 
-        data = doc.as_ref().unwrap().search(&query);
-        max_height = data.len();
-        let _ = term.clear();
-        for (i, v) in data.iter().enumerate() {
-            let _ = term.print(i + 1, 0, v);
-        }
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
 
-        let display_query = format!("> {}", query);
-        let _ = term.print(0, 0, &display_query);
-        let _ = term.set_cursor(0, display_query.len());
-
-        if let Some(val) = data.get(row - 1) {
-            let attr = Attr {
-                bg: Color::LIGHT_BLACK,
-                ..Attr::default()
-            };
-            let _ = term.print_with_attr(row, 0, &*val, attr);
-        } else if data.is_empty() {
-            let _ = term.clear();
-            let display_query = format!("> {}", query);
-            let _ = term.print(0, 0, &display_query);
-            let _ = term.set_cursor(0, display_query.len());
-            let _ = term.print(1, 0, &format!("Query '{}' not found!", query));
-        }
-        let _ = term.present();
+    match res {
+        Ok(app) => app.run_script(),
+        Err(err) => println!("{:?}", err),
     }
+
+    Ok(())
 }
